7d7b0da1056d8c4ba0cd55724bd6569a64eb35ea
cheesysteak <andmichaelis1@gmail.com>
2018-05-14 17:44:46 -0500

    add more multiplayer info

---
+++ MoreMultiplayerInfo/ModEntry.cs
@@ -0,0 +1,14 @@
+﻿using StardewModdingAPI;
+
+namespace MoreMultiplayerInfo
+{
+    public class ModEntry : Mod
+    {
+        private ModEntryHelper _baseHandler;
+
+        public override void Entry(IModHelper helper)
+        {
+            _baseHandler = new ModEntryHelper(Monitor, helper);
+        }
+    }
+}
---
+++ MoreMultiplayerInfo/ModEntryHelper.cs
@@ -0,0 +1,17 @@
+﻿using StardewModdingAPI;
+
+namespace MoreMultiplayerInfo
+{
+    public class ModEntryHelper
+    {
+        public ModEntryHelper(IMonitor monitor, IModHelper modHelper)
+        {
+            var showIcon = new ShowPlayerIconHandler(monitor, modHelper);
+
+            // var logHandler = new LogInputHandler(monitor, modHelper);
+        }
+
+
+
+    }
+}
